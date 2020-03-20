3043fa4bf8d1a8a24554b896e5cc0583cf2aa196
SANCHEZ <esanch20@visteon.com>
2020-03-20 15:55:31 -0600

    Cmake Fix

--- buildWin.bat
+++ buildWin.bat
@@ -1,2 +1,4 @@
-
-cmake ../ -G "Unix Makefiles"
+set PATH=%PATH%;C:\Python27
+mkdir build_win
+cd build_win
+cmake ../ -G "Unix Makefiles" -DESP_TOOLCHAIN_PATH=C:/Users/esanch20/Documents/00_Projects/04_Git/IOT_ESP/arduino-esp32
